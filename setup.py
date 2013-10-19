e4e4c917078d8174987cbced50a8d7114f497e52
Kyle Treleaven <treleaven.kyle@gmail.com>
2013-10-19 08:22:11 -0400

    packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+
+from setuptools import setup, find_packages
+
+setup(
+	name = "nxopt",
+	description = "A pretty ugly network optimization library, more for convenience than anything",
+	author = "Kyle Treleaven",
+	author_email = "ktreleav@gmail.com",
+	version = "0.0.0",
+	packages = find_packages(),
+	namespace_packages = [ 'setiptah', ],
+)
+
